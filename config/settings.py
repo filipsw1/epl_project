8dc386ea44501c5b530ce6a0102959888bdd174a
Filip SW <filipsw1@gmail.com>
2026-01-09 17:44:42 +0100

    added colours for positions in the table and changed problematic colors to once that worked

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,20 @@ LOGS_DIR.mkdir(exist_ok=True)
 # api config, behövs skyddas 
 API_KEY = "ae048f6fa27a448cad56f41748b7c0ae"
 API_URL = "https://api.football-data.org/v4/competitions/PL/standings"
+API_MATCHES_URL = "https://api.football-data.org/v4/competitions/PL/matches"
 
 LOG_FILE = LOGS_DIR / "project.log"
 
 FORM_COLORS = {
-    "W": "\033[92m]", # grön
-    "L": "\033[91m]", # röd
-    "D": "\033[90m]" # grå
+    "W": "\x1b[32m", # grön
+    "L": "\x1b[31m", # röd
+    "D": "\x1b[90m", # grå
+    "RESET": "\x1b[0m"
+}
+
+POSITION_COLORS = {
+    "CL": "\033[48;5;22m",     
+    "EL": "\033[48;5;94m",     
+    "REL": "\033[48;5;52m",     
+    "RESET": "\033[0m"
 }
