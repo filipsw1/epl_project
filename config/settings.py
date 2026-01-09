b238264dfbb07b57e3447ae79e6000408f44d1dd
Filip SW <filipsw1@gmail.com>
2026-01-09 10:21:19 +0100

    added config module for the api

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+# projektets rot
+PROJECT_ROOT = Path(__file__).parent.parent
+
+#directory till logs
+LOGS_DIR = PROJECT_ROOT / "logs"
+LOGS_DIR.mkdir(exist_ok=True)
+
+# api config, behövs skyddas 
+API_KEY = "ae048f6fa27a448cad56f41748b7c0ae"
+API_URL = "https://api.football-data.org/v4/competitions/PL/standings"
+
+LOG_FILE = LOGS_DIR / "project.log"
